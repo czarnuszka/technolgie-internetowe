c3854930001dae1fe83fc379a1084ac04319f2fb
Gabriela Sygnarowska <gabriela.sygnarowska@outlook.com>
2020-06-22 23:04:10 +0200

    Galeria część 2

--- scripts/gallery_upload.php
+++ scripts/gallery_upload.php
@@ -15,7 +15,7 @@
         //FILES zawiera dane na temat pliku przesyłanego za pomocą kodu. Nie zawiera przesłanego pliku, a inną tablicę
         $file = $_FILES["file"];
 
-        //
+        //zmienne dotyczące pliku
         $fileName = $file['name'];
         $fileType = $file['type'];
         $fileTempName = $file['temp_name'];
@@ -28,5 +28,36 @@
 
         //Rozszerzenia z jakimi można udostępniać pliki
         $allowed = array("jpg", "jpeg", "png");
+
+        if (in_array($fileActualExt, $allowed)) {
+            if ($fileError === 0) {
+                if ($fileSize < 2000000) {
+                    //nazwa pliku . unikatowe id . rozszerzenie
+                    $imageFullName = $newFileName . "." . uniqid("", true) . "." . $fileActualExt;
+                    $fileDestination = "../img/gallery/" . $imageFullName;
+
+                    //połączenie z bazą galerii
+                    include_once "config/databaseGallery.php";
+
+                    if (empty($imageTitle) || empty($imageDesc)) {
+                        header("Location: ../gallery.php?upload=empty");
+                        exit();
+                    } else {
+                        $sql = "SELECT * FROM gallery;";
+                        $stmt = mysqli_stmt_init($conn);
+                        
+                    }
+                } else {
+                    echo "File size is too big!";
+                    exit();
+                }
+            } else {
+                echo "You had aan error!";
+                exit();
+            }
+        } else {
+            echo "You need to upload a proper file type";
+            exit();
+        }
 
     }
